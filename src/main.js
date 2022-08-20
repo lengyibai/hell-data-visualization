5ffd96d297d4ee15b23b16c6ba830635fbf0d143
lengyibai <1329670984@qq.com>
2022-08-21 02:39:49 +0800

    增加CSS样式

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 // import store from "./store";
-import "@/assets/css/index.css";
+import "@/style/clear.css";
 import "@/utils/flexible.js";
+import "@/style/css-variable.css";
 
 Vue.config.productionTip = false;
 
