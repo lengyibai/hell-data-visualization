90bd1c58cfcd44a17e0a011c1257462fa6477481
lengyibai <1329670984@qq.com>
2022-08-22 23:51:01 +0800

    使用图表组件

--- src/views/Home/childComps/HomeBottom/childComps/BottomFour/index.vue
+++ src/views/Home/childComps/HomeBottom/childComps/BottomFour/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="BottomFour">
-    <CardTitle title="阴司数据中心" />
+    <CardTitle title="阴司业务中心" />
+    <div class="chart">
+      <E-Pie-A />
+    </div>
   </div>
 </template>
 <script>
@@ -15,8 +18,13 @@ export default {
 </script>
 <style scoped lang="less">
 .BottomFour {
+  display: flex;
+  flex-direction: column;
   position: relative;
   flex: 1;
   height: 100%;
+  .chart {
+    flex: 1;
+  }
 }
 </style>
