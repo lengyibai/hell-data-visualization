af617297b5e8bf3b6fc951d122fc8e121e5c8729
lengyibai <1329670984@qq.com>
2022-08-22 17:53:37 +0800

    使用柱状图

--- src/views/Home/childComps/HomeCenter/childComps/CenterRight/childComps/RightTop/index.vue
+++ src/views/Home/childComps/HomeCenter/childComps/CenterRight/childComps/RightTop/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="RightTop">
     <CardTitle title="地府客流统计" border center />
+    <div class="tab"></div>
+    <div class="chart">
+      <E-Line-A />
+    </div>
   </div>
 </template>
 <script>
@@ -15,9 +19,22 @@ export default {
 </script>
 <style scoped lang="less">
 .RightTop {
+  display: flex;
+  flex-direction: column;
   position: relative;
   width: 100%;
   height: calc(50% - 5px);
   border: 1px solid var(--color-two);
+  .tab {
+    height: 45px;
+    background-color: red;
+  }
+  .chart {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    flex: 1;
+  }
 }
 </style>
