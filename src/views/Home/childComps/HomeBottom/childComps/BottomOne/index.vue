be23550dd5f84b22c6c44973faec520da65241aa
lengyibai <1329670984@qq.com>
2022-08-22 18:28:38 +0800

    使用漏斗组件

--- src/views/Home/childComps/HomeBottom/childComps/BottomOne/index.vue
+++ src/views/Home/childComps/HomeBottom/childComps/BottomOne/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="BottomOne">
     <CardTitle title="阴司飘员构成" />
+    <div class="info"></div>
+    <div class="chart">
+      <E-Funnel-A />
+    </div>
   </div>
 </template>
 <script>
@@ -15,10 +19,23 @@ export default {
 </script>
 <style scoped lang="less">
 .BottomOne {
+  display: flex;
+  flex-direction: column;
   position: relative;
   // width: 485px;
   flex: 1;
   height: 100%;
   border-right: 1px solid var(--color-two);
+  .info {
+    height: 45px;
+  }
+  .chart {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    flex: 1;
+    background-color: #000;
+  }
 }
 </style>
