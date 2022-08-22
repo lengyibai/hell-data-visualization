d81aacd0cd3ef83031635db07d69df5190380e32
lengyibai <1329670984@qq.com>
2022-08-22 23:51:44 +0800

    增加网格背景

--- src/views/Home/childComps/HomeCenter/childComps/CenterCenter/index.vue
+++ src/views/Home/childComps/HomeCenter/childComps/CenterCenter/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="CenterCenter"></div>
+  <div class="CenterCenter">
+    <div class="bg"></div>
+    <E-Map />
+  </div>
 </template>
 <script>
 export default {
@@ -18,5 +21,17 @@ export default {
   height: 100%;
   margin: 0 0.0521rem;
   border: 0.0052rem solid var(--color-two);
+  .bg {
+    position: absolute;
+    inset: 0;
+    width: 100%;
+    height: 100%;
+    background: url("./img/bg.png") no-repeat center center;
+    background-size: cover;
+    z-index: 1;
+    pointer-events: none;
+    mask: radial-gradient(#fff -20%, transparent 75%);
+    opacity: 0.1;
+  }
 }
 </style>
