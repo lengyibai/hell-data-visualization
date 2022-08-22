bb0fa1d16cf4acdc608a1954e562582d15c917fa
lengyibai <1329670984@qq.com>
2022-08-22 16:25:48 +0800

    使用雷达组件

--- src/views/Home/childComps/HomeCenter/childComps/CenterLeft/childComps/LeftBottom/index.vue
+++ src/views/Home/childComps/HomeCenter/childComps/CenterLeft/childComps/LeftBottom/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="LeftBottom"></div>
+  <div class="LeftBottom">
+    <CardTitle title="阴司勾魂系统" border center />
+    <div class="chart">
+      <E-Radar class="E-Radar" />
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -13,9 +18,19 @@ export default {
 </script>
 <style scoped lang="less">
 .LeftBottom {
+  display: flex;
+  flex-direction: column;
   position: relative;
   width: 100%;
   height: calc(50% - 5px);
-  border: 1px solid #73baf8;
+  border: 1px solid var(--color-two);
+  .chart {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 1.0417rem;
+    background-color: #000;
+  }
 }
 </style>
