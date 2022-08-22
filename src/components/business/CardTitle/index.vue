20b6d9b7d45ab46daf170740a3ebd84bfdbe6b09
lengyibai <1329670984@qq.com>
2022-08-22 16:24:28 +0800

    容器标题组件

---
+++ src/components/business/CardTitle/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="CardTitle" :class="{ border: border, center: center }">
+    {{ title }}
+  </div>
+</template>
+<script>
+export default {
+  name: "CardTitle",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    border: {
+      type: Boolean,
+      default: false,
+    },
+    center: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
+};
+</script>
+<style scoped lang="less">
+.CardTitle {
+  display: flex;
+  height: 0.2083rem;
+  font-size: 0.1146rem;
+  color: var(--color-one);
+  padding-left: 1em;
+}
+.border {
+  border-bottom: 1px solid var(--color-two);
+}
+.center {
+  align-items: center;
+}
+</style>
